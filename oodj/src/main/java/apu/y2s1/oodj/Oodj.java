a4edd25e2bbc3db4e8655de2dc1d2659479791c3
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2024-03-27 09:47:03 +0800

    Initial commit

---
+++ oodj/src/main/java/apu/y2s1/oodj/Oodj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package apu.y2s1.oodj;
+
+/**
+ *
+ * @author tenen
+ */
+public class Oodj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
