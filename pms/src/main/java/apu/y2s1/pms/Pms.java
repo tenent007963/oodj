2f2f33bb9de4ddefd086c9ee2544baa3ebce1e80
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2024-04-08 14:41:56 +0800

    Initial commit

---
+++ pms/src/main/java/apu/y2s1/pms/Pms.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package apu.y2s1.pms;
+
+/**
+ *
+ * @author tenen
+ */
+public class Pms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
