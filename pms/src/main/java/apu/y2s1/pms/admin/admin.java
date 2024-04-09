9997ef6dd45ea6c7319397e9e197e9c2d8d5f6ae
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2024-04-09 12:31:18 +0800

    admin

---
+++ pms/src/main/java/apu/y2s1/pms/admin/admin.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apu.y2s1.pms.admin;
+
+/**
+ *
+ * @author tenen
+ */
+public class admin {
+    
+}
