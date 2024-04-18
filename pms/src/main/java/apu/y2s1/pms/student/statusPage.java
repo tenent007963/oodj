6a05299d34b0773c2f7b718a913971df503a63d5
User <User@LAPTOP-TF73GKB8>
2024-04-18 15:08:04 +0800

    kenny

--- pms/src/main/java/apu/y2s1/pms/student/statusPage.java
+++ pms/src/main/java/apu/y2s1/pms/student/statusPage.java
@@ -36,6 +36,7 @@ public class statusPage extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setBackground(new java.awt.Color(242, 242, 242));
