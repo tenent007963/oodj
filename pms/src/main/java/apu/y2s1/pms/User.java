97d7656fe48707ef58042ee6f529ed049d40d738
User <User@LAPTOP-TF73GKB8>
2024-05-18 12:17:21 +0800

    kenny

--- pms/src/main/java/apu/y2s1/pms/User.java
+++ pms/src/main/java/apu/y2s1/pms/User.java
@@ -9,11 +9,11 @@ package apu.y2s1.pms;
  * @author Jeslyn
  */
 class User {
-    protected static User instance;
-    protected String userID;
-    protected String role;
-    protected String filename;
-    protected DataAbstract db;
+    private static User instance;
+    private String userID;
+    private String role;
+    private String filename;
+    private DataAbstract db;
     
     public User(String ID, String role) {
         this.role = role;
