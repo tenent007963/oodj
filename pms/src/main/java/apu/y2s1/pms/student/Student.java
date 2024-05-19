936e22238a1b94e5efb1d90a4af2453558e28f82
User <User@LAPTOP-TF73GKB8>
2024-05-19 16:02:38 +0800

    kenny

--- pms/src/main/java/apu/y2s1/pms/student/Student.java
+++ pms/src/main/java/apu/y2s1/pms/student/Student.java
@@ -1,17 +1,17 @@
 package apu.y2s1.pms.student;
 import apu.y2s1.pms.User;
 
-public class Student extends User {
+public class Student extends User { /*inherit user class*/
     private String intake_code;
     private String assessment_assigned;
     
     public Student(String ID) {
-        super(ID, "student");
+        super(ID, "student"); /*inherit user class*/
         getAddiData();
     }
 
     public void getAddiData(){
-        String[] rawdata = db.getRow(super.intID);
+        String[] rawdata = db.getRow(intID);
         this.intake_code = rawdata[4];
         this.assessment_assigned = rawdata[5];
     }
