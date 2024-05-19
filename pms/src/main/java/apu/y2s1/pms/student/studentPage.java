3841cef97fb80607f6d27ff08ccd30e2ed4a411c
User <User@LAPTOP-TF73GKB8>
2024-05-19 16:01:11 +0800

    kenny

--- pms/src/main/java/apu/y2s1/pms/student/studentPage.java
+++ pms/src/main/java/apu/y2s1/pms/student/studentPage.java
@@ -193,7 +193,7 @@ public class studentPage extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new studentPage(user).setVisible(true);
+                new studentPage().setVisible(true);
             }
         });
     }
